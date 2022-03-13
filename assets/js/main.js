1eb9b232b04a0c583a57259fac7be52d9a28136c
Luiz Fernando <luizfvidal13@hotmail.com>
2022-03-13 10:15:50 -0300

    projeto alterado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,84 +1,85 @@
-function list() {
-	const listValue = document.querySelector(".add input");
-	const btnAdd = document.querySelector(".add button");
-	const select = document.querySelector("select");
+const texto = document.querySelector('input')
+const btnInsert = document.querySelector('.divInsert button')
+const btnDeleteAll = document.querySelector('.header button')
+const ul = document.querySelector('ul')
 
-	btnAdd.addEventListener("click", add);
-	listValue.addEventListener("keyup", (e) => (e.keyCode == 13 ? add() : false));
-	document.addEventListener("click", (e) => removeAndCompleted(e));
-	select.addEventListener("click", (e) => fillter(e));
+var itensDB = []
 
-	const removeAndCompleted = (e) => {
-		let target = e.target;
-		if (target.classList.contains("remove")) {
-			clickEffect(target);
-			removeList(target);
-			listValue.focus();
-		} else if (target.classList.contains("done")) {
-			clickEffect(target);
-			doneList(target);
-			listValue.focus();
-		}
-	};
-
-	const fillter = (e) => {
-		const todos = document.querySelectorAll(".list");
-		todos.forEach((todo) => {
-			switch (e.target.value) {
-				case "all":
-					todo.style.display = "flex";
-					break;
-				case "completed":
-					todo.classList.contains("completed") ? (todo.style.display = "flex") : (todo.style.display = "none");
-					break;
-				case "uncompleted":
-					!todo.classList.contains("completed") ? (todo.style.display = "flex") : (todo.style.display = "none");
-					break;
-			}
-		});
-	};
-
-	function add() {
-		if (listValue.value == "") return;
-		createList();
-		listValue.setAttribute("placeholder", "Digite a próxima!");
-		listValue.value = "";
-		listValue.focus();
-	}
-
-	const createList = () => {
-		const listContainer = document.querySelector(".list-container");
-		const div = document.createElement("div");
-		div.classList.add("list", "loading");
-		div.innerHTML = listElement();
-		listContainer.appendChild(div);
-		setTimeout(() => div.classList.remove("loading"), 600);
-	};
-
-	const removeList = (target) => {
-		const list = target.parentElement.parentElement;
-		list.classList.add("removed");
-		list.classList.remove("completed");
-		setTimeout(() => list.remove(), 600);
-	};
-
-	const doneList = (target) => {
-		const list = target.parentElement.parentElement;
-		list.classList.toggle("completed");
-	};
-
-	const listElement = () => {
-		return `<p>${listValue.value}</p>
-        <div class="button">
-            <button class="done"><i class="fas fa-check"></i></button>
-            <button class="remove"><i class="fas fa-trash"></i></button>
-        </div>`;
-	};
-
-	const clickEffect = (target) => {
-		target.style.opacity = ".7";
-		setTimeout(() => (target.style.opacity = "1"), 100);
-	};
+btnDeleteAll.onclick = () => {
+  itensDB = []
+  updateDB()
 }
 
-list();
+texto.addEventListener('keypress', e => {
+  if (e.key == 'Enter' && texto.value != '') {
+    setItemDB()
+  }
+})
+
+btnInsert.onclick = () => {
+  if (texto.value != '') {
+    setItemDB()
+  }
+}
+
+function setItemDB() {
+  if (itensDB.length >= 20) {
+    alert('Limite máximo de 20 itens atingido!')
+    return
+  }
+
+  itensDB.push({ 'item': texto.value, 'status': '' })
+  updateDB()
+}
+
+function updateDB() {
+  localStorage.setItem('todolist', JSON.stringify(itensDB))
+  loadItens()
+}
+
+function loadItens() {
+  ul.innerHTML = "";
+  itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
+  itensDB.forEach((item, i) => {
+    insertItemTela(item.item, item.status, i)
+  })
+}
+
+function insertItemTela(text, status, i) {
+  const li = document.createElement('li')
+  
+  li.innerHTML = `
+    <div class="div-li">
+      <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i});" />
+      <span data-si=${i}>${text}</span>
+      <button class="btn btn-sm" onclick="removeItem(${i})" data-i=${i}><i class='fas fa-trash text-danger'></i></button>
+    </div>
+    `
+  ul.appendChild(li)
+
+  if (status) {
+    document.querySelector(`[data-si="${i}"]`).classList.add('line-through')
+  } else {
+    document.querySelector(`[data-si="${i}"]`).classList.remove('line-through')
+  }
+
+  texto.value = ''
+}
+
+function done(chk, i) {
+
+  if (chk.checked) {
+    itensDB[i].status = 'checked' 
+  } else {
+    itensDB[i].status = '' 
+  }
+
+  updateDB()
+}
+
+function removeItem(i) {
+  itensDB.splice(i, 1)
+  updateDB()
+}
+
+loadItens()
